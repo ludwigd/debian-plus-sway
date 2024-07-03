3ee885a38c7433461101db929dd1300b989065cc
Damian Ludwig <ludwigd@fedoraproject.org>
2024-07-03 08:29:10 +0200

    feat: no flatpak

--- install.sh
+++ install.sh
@@ -4,40 +4,40 @@ set -eo  pipefail
 # Wifi-support and basic networking tools
 subtask_network () {
     apt install --assume-yes \
-	bluez \
-	firewalld \
-	network-manager
+        bluez \
+        firewalld \
+        network-manager
 }
 
 # Install pipewire along with some tools
 subtask_audio () {
     apt install --assume-yes \
-	pavucontrol \
-	pipewire-audio \
-	playerctl \
-	pulseaudio-utils
+        pavucontrol \
+        pipewire-audio \
+        playerctl \
+        pulseaudio-utils
 }
 
 # Printing support
 subtask_printing () {
     apt install --assume-yes \
-	cups \
-	ghostscript \
-	hplip \
-	libsane-hpaio \
-	system-config-printer
+        cups \
+        ghostscript \
+        hplip \
+        libsane-hpaio \
+        system-config-printer
 }
 
 # Fonts
 subtask_fonts () {
     apt install --assume-yes \
-	fontconfig \
-	fonts-dejavu \
-	fonts-font-awesome \
-	fonts-jetbrains-mono \
-	fonts-liberation2 \
-	fonts-noto-cjk \
-	fonts-noto-color-emoji
+        fontconfig \
+        fonts-dejavu \
+        fonts-font-awesome \
+        fonts-jetbrains-mono \
+        fonts-liberation2 \
+        fonts-noto-cjk \
+        fonts-noto-color-emoji
 }
 
 # Desktop
@@ -48,31 +48,31 @@ task_desktop () {
     subtask_printing
     
     apt install --assume-yes \
-	clipman \
-	desktop-base \
-	findutils \
-	fish \
-	gammastep \
-	git-core \
-	gnome-keyring \
-	i3status \
-	jq \
-	kanshi \
-	libpam-gnome-keyring \
-	light \
-	mate-polkit \
-	python3-i3ipc \
-	rofi \
-	sway \
-	swayidle \
-	swaylock \
-	vim \
-	wev \
-	x11-utils \
-	xdg-desktop-portal-gtk \
-	xdg-desktop-portal-wlr \
-	xdg-utils \
-	xwayland
+        clipman \
+        desktop-base \
+        findutils \
+        fish \
+        gammastep \
+        git-core \
+        gnome-keyring \
+        i3status \
+        jq \
+        kanshi \
+        libpam-gnome-keyring \
+        light \
+        mate-polkit \
+        python3-i3ipc \
+        rofi \
+        sway \
+        swayidle \
+        swaylock \
+        vim \
+        wev \
+        x11-utils \
+        xdg-desktop-portal-gtk \
+        xdg-desktop-portal-wlr \
+        xdg-utils \
+        xwayland
 
     LOCAL_BIN_DIR=/usr/local/bin
     mkdir -p $LOCAL_BIN_DIR
@@ -87,108 +87,104 @@ task_desktop () {
 # Extra desktop apps
 task_apps () {
     apt install --assume-yes \
-	adwaita-icon-theme \
-	android-file-transfer \
-	android-sdk-platform-tools \
-	borgbackup \
-	distrobox \
-	emacs \
-	flatpak \
-	gnome-icon-theme \
-	gnome-themes-extra \
-	htop \
-	imv \
-	lynx \
-	neomutt \
-	podman \
-	powertop \
-	ranger \
-	sshfs \
-	thunar \
-	udiskie \
-	virt-manager \
-	xsane \
-	zathura \
-	zathura-ps
-
-    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    flatpak remote-modify --enable flathub
-	
-    flatpak install -y flathub \
-	    com.github.xournalpp.xournalpp \
-	    io.github.quodlibet.QuodLibet \
-	    io.mpv.Mpv \
-	    org.chromium.Chromium \
-	    org.gimp.GIMP \
-	    org.inkscape.Inkscape \
-	    org.keepassxc.KeePassXC \
-	    org.libreoffice.LibreOffice \
-	    org.mozilla.firefox
+        adwaita-icon-theme \
+        android-file-transfer \
+        android-sdk-platform-tools \
+        borgbackup \
+        chromium \
+        distrobox \
+        emacs \
+        ffmpeg \
+        firefox-esr \
+        gimp \
+        gnome-icon-theme \
+        gnome-themes-extra \
+        gvfs-backends \
+        htop \
+        imv \
+        inkscape \
+        keepassxc \
+        libreoffice \
+        libreoffice-gtk3 \
+        lynx \
+        mpv \
+        neomutt \
+        podman \
+        powertop \
+        quodlibet \
+        sshfs \
+        thunar \
+        udiskie \
+        virt-manager \
+        xournalpp \
+        xsane \
+        zathura \
+        zathura-ps
 }
 
 # Development Tools
 task_development () {
     apt install --assume-yes \
-	autoconf \
-	automake \
-	bc \
-	binutils \
-	bison \
-	cargo \
-	cmake \
-	exuberant-ctags \
-	flex \
-	g++ \
-	gcc \
-	gdb \
-	git \
-	javacc \
-	libglib2.0-dev \
-	make \
-	openjdk-17-jdk \
-	openjdk-17-jre \
-	patch \
-	patchutils \
-	python3 \
-	python3-pip \
-	python3-virtualenv \
-	rustc \
-	strace \
-	zstd
+        autoconf \
+        automake \
+        bc \
+        binutils \
+        bison \
+        cargo \
+        cmake \
+        exuberant-ctags \
+        flex \
+        g++ \
+        gcc \
+        gdb \
+        git \
+        javacc \
+        libglib2.0-dev \
+        make \
+        openjdk-17-jdk \
+        openjdk-17-jre \
+        patch \
+        patchutils \
+        python3 \
+        python3-pip \
+        python3-virtualenv \
+        rustc \
+        strace \
+        zstd
 }
 
 # TeXlive and publishing
 task_publishing () {
     apt install --assume-yes \
-	aspell \
-	aspell-de \
-	aspell-en \
-	hunspell \
-	hunspell-de-de \
-	imagemagick \
-	pandoc \
-	texlive-base \
-	texlive-bibtex-extra \
-	texlive-extra-utils \
-	texlive-font-utils \
-	texlive-fonts-extra \
-	texlive-fonts-recommended \
-	texlive-formats-extra \
-	texlive-lang-english \
-	texlive-lang-german \
-	texlive-latex-base \
-	texlive-latex-extra \
-	texlive-latex-recommended \
-	texlive-luatex \
-	texlive-metapost \
-	texlive-pictures \
-	texlive-plain-generic \
-	texlive-pstricks \
-	texlive-publishers \
-	texlive-science \
-	texlive-xetex \
-	texstudio \
-	xfig
+        aspell \
+        aspell-de \
+        aspell-en \
+        hunspell \
+        hunspell-de-de \
+        imagemagick \
+        pandoc \
+        texlive-base \
+        texlive-bibtex-extra \
+        texlive-extra-utils \
+        texlive-font-utils \
+        texlive-fonts-extra \
+        texlive-fonts-recommended \
+        texlive-formats-extra \
+        texlive-lang-english \
+        texlive-lang-german \
+        texlive-latex-base \
+        texlive-latex-extra \
+        texlive-latex-recommended \
+        texlive-luatex \
+        texlive-metapost \
+        texlive-pictures \
+        texlive-plain-generic \
+        texlive-pstricks \
+        texlive-publishers \
+        texlive-science \
+        texlive-xetex \
+        texstudio \
+        xfig
 }
 
 # Updates
@@ -196,7 +192,7 @@ task_update () {
     apt update
     apt upgrade --assume-yes
     if [[ -f "/usr/bin/flatpak" ]]; then
-	flatpak -y update
+        flatpak -y update
     fi
 }
 
@@ -204,7 +200,7 @@ task_update () {
 task_dotfiles () {
     # Check dependencies
     if [[ ! $(which git) ]]; then
-	exit 1
+        exit 1
     fi
     
     # Clone the repository
@@ -238,8 +234,8 @@ usage () {
 
 assure_root () {
     if [ $UID -ne 0 ]; then
-	echo "You must be root to install software."
-	exit 1
+        echo "You must be root to install software."
+        exit 1
     fi
 }
 
@@ -247,50 +243,50 @@ main () {
     local cmd=$1
 
     if [[ -z "$cmd" ]]; then
-	usage
+        usage
     elif [[ $cmd == "desktop" ]]; then
-	assure_root
-	task_desktop
+        assure_root
+        task_desktop
     elif [[ $cmd == "apps" ]]; then
-	assure_root
-	task_apps
+        assure_root
+        task_apps
     elif [[ $cmd == "development" ]]; then
-	assure_root
-	task_development
+        assure_root
+        task_development
     elif [[ $cmd == "publishing" ]]; then
-	assure_root
-	task_publishing
+        assure_root
+        task_publishing
     elif [[ $cmd == "dotfiles" ]]; then
-	if [ $UID -ne 0 ]; then
-	    task_dotfiles
-	else
-	    echo "You should NOT be root for this task."
-	    exit 1
-	fi
+        if [ $UID -ne 0 ]; then
+            task_dotfiles
+        else
+            echo "You should NOT be root for this task."
+            exit 1
+        fi
     elif [[ $cmd == "update" ]]; then
-	assure_root
-	task_update
+        assure_root
+        task_update
     elif [[ $cmd == "everything" ]]; then
-	assure_root
-	task_update
-	task_desktop
-	task_apps
-	task_development
-	task_publishing
+        assure_root
+        task_update
+        task_desktop
+        task_apps
+        task_development
+        task_publishing
 
-	# Who am I?
-	ME=$(who am i | cut -f1 -d" ")
+        # Who am I?
+        ME=$(who am i | cut -f1 -d" ")
 
-	# Add user to libvirt group
-	usermod -aG libvirt $ME
+        # Add user to libvirt group
+        usermod -aG libvirt $ME
 
-	# Install dotfiles
-	sudo -u $ME ./"$0" dotfiles
+        # Install dotfiles
+        sudo -u $ME ./"$0" dotfiles
 
-	# Reboot
-	systemctl reboot
+        # Reboot
+        systemctl reboot
     else
-	usage
+        usage
     fi
 }
 
